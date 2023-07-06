b265907230e4e1ae084af9cd9065642deee0fb29
AndroXcta <alejocarmonah@gmail.com>
2023-07-05 22:45:19 -0500

    cree otra carpeta con ejeercicios

---
+++ 04-Frontend/java_script/actividades/01-array-numeroMasGrande/script.js
@@ -0,0 +1,69 @@
+/*const numeros = [5, 2, 1, 9, 8, 4, 7]
+let ayuda = numeros [3]
+
+
+for (let i = 0; i < numeros.length; i++) {
+    if (ayuda <= numeros[i]) {
+        console.log ("el numero mayor es", numeros [i])
+    }
+
+    
+}
+
+
+const numeros = [5, 7, 10, 4, 8, 9, 10, 15, 20, 3];
+let ayuda = numeros[0];
+
+for (let i = 0; i < numeros.length; i++) {
+  if (numeros[i] > ayuda) {
+    ayuda = numeros[i];
+  }
+
+}
+
+console.log('El número mayor es:', ayuda);
+
+*/
+
+
+function numeroMayor(array) {
+    if (!Array.isArray(array)) {
+        console.log("Solo se admiten arrays");
+        return; 
+      }
+      if (array.some(isNaN)) {
+        console.log("Solo se admiten números");
+        return; 
+      }
+    
+    let ayuda = array[0];
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] > ayuda) {
+          ayuda = array[i];
+        }
+      }
+    console.log('El número mayor es:', ayuda);
+     
+}
+
+
+
+function numeroMenor (array) {
+    let ayuda = array[0];
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] < ayuda) {
+          ayuda = array[i];
+        }
+      }
+    console.log('El número menor es:', ayuda);
+     
+}
+
+
+
+
+
+
+
+
+
